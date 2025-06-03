4b55c6272b1b905d76ebb70df7ffc8f9d0c02f91
rmusukudabbidi <rmusukudabbidi@slu.edu>
2025-06-03 16:02:40 -0500

    Initial commit

---
+++ js/home.js
@@ -0,0 +1,13 @@
+// js/home.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById('menu-toggle');
+    const mobileMenu = document.getElementById('mobile-menu');
+  
+    if (menuToggle && mobileMenu) {
+      menuToggle.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden');
+      });
+    }
+  });
+  
